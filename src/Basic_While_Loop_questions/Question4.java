bcd16c8bf6ca662ecaf08b6d85afee2a0923b4ea
Hatim Husain <hatim.husain24@gmail.com>
2026-06-01 23:50:08 +0530

    Question 4 : print all odd numbers between 1 and 100

---
+++ src/Basic_While_Loop_questions/Question4.java
@@ -0,0 +1,19 @@
+package Basic_While_Loop_questions;
+
+//print all odd numbers between 1 and 100
+public class Question4 {
+    public static void main(String[] args) {
+        int i=1;
+        while (i<=100){
+            if(i%2 != 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+        int j = 1;
+        while (j<=100){
+            System.out.println(j);
+            j=j+2;
+        }
+    }
+}
