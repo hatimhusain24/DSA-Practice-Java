eac733840f7870e09013c5d29464a3734ccdca65
Hatim Husain <hatim.husain24@gmail.com>
2026-06-01 23:49:14 +0530

    Question 1 : print all numbers from 1 to 10 using a loop

---
+++ src/Basic_While_Loop_questions/Question1.java
@@ -0,0 +1,12 @@
+package Basic_While_Loop_questions;
+
+//print all numbers from 1 to 10 using a loop
+public class Question1 {
+    public static void main(String[] args) {
+        int i=1;
+        while(i <=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
