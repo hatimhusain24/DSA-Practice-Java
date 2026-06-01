0ebc252d5f2201c8fba8c15b7652c46dfef3059b
Hatim Husain <hatim.husain24@gmail.com>
2026-06-01 23:49:41 +0530

    Question 2 : print numbers from 10 down to 1 in reverse order

---
+++ src/Basic_While_Loop_questions/Question2.java
@@ -0,0 +1,12 @@
+package Basic_While_Loop_questions;
+
+//print numbers from 10 down to 1 in reverse order
+public class Question2 {
+    public static void main(String[] args) {
+        int i=10;
+        while (i>0){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
