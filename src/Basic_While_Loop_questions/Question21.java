9dfd7815b23241e761e9c2f468cacd91db0fcbf1
Hatim Husain <hatim.husain24@gmail.com>
2026-06-04 00:21:46 +0530

    Question 21 : print the square of each number from 1 to n

---
+++ src/Basic_While_Loop_questions/Question21.java
@@ -0,0 +1,16 @@
+package Basic_While_Loop_questions;
+
+import java.util.Scanner;
+
+//print the square of each number from 1 to n
+public class Question21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        int i = 1;
+        while (i <= n){
+            System.out.println(i*i);
+            i++;
+        }
+    }
+}
