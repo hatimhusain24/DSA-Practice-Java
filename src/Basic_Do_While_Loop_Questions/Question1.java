e88f40370a30c3d11a4ebd27c2f80de2c659eebd
Hatim Husain <hatim.husain24@gmail.com>
2026-06-04 23:55:20 +0530

    Question 1 do while : print all numbers from 1 to 10

---
+++ src/Basic_Do_While_Loop_Questions/Question1.java
@@ -0,0 +1,12 @@
+package Basic_Do_While_Loop_Questions;
+
+//print all numbers from 1 to 10
+public class Question1 {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<=10);
+    }
+}
