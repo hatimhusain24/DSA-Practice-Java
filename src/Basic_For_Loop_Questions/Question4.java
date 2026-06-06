e50554fe578dbb45f988ecfb2a0be3d079a810e2
Hatim Husain <hatim.husain24@gmail.com>
2026-06-06 21:35:56 +0530

    Question 4 for loop : print all odd numbers between 1 and 100

---
+++ src/Basic_For_Loop_Questions/Question4.java
@@ -0,0 +1,10 @@
+package Basic_For_Loop_Questions;
+
+//print all odd numbers between 1 and 100
+public class Question4 {
+    public static void main(String[] args) {
+        for (int i=1; i<100; i+=2){
+            System.out.println(i);
+        }
+    }
+}
