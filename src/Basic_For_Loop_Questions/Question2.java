e39b4cf1bdc86e006e0c83c2c44d7bcf8b48f90a
Hatim Husain <hatim.husain24@gmail.com>
2026-06-06 21:30:59 +0530

    Question 2 for loop : print numbers from 10 down to 1 in reverse order

---
+++ src/Basic_For_Loop_Questions/Question2.java
@@ -0,0 +1,10 @@
+package Basic_For_Loop_Questions;
+
+//print numbers from 10 down to 1 in reverse order
+public class Question2 {
+    public static void main(String[] args) {
+        for (int i=10; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
