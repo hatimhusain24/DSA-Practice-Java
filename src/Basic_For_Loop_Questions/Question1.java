0bcd7584cc071ffe9fc01477d837cc139d25cdbb
Hatim Husain <hatim.husain24@gmail.com>
2026-06-06 21:29:58 +0530

    Question 1 for loop : print all numbers from 1 to 10

---
+++ src/Basic_For_Loop_Questions/Question1.java
@@ -0,0 +1,10 @@
+package Basic_For_Loop_Questions;
+
+//print all numbers from 1 to 10
+public class Question1 {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
