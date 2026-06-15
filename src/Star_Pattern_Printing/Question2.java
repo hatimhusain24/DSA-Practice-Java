45f2fa3f8fcc131ebeebc21f9308146ddc2e4c4a
Hatim Husain <hatim.husain24@gmail.com>
2026-06-15 23:55:45 +0530

    Question 2 Star Pattern Printing : print four star

---
+++ src/Star_Pattern_Printing/Question2.java
@@ -0,0 +1,10 @@
+package Star_Pattern_Printing;
+
+//print four star
+public class Question2 {
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++){
+            System.out.print("*");
+        }
+    }
+}
