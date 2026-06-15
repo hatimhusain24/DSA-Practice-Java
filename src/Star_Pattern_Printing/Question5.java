97fa6b9996eb5047e1f54379195f5a4d22384f87
Hatim Husain <hatim.husain24@gmail.com>
2026-06-16 00:00:39 +0530

    Question 5 Star Pattern Printing : print an increasing triangle of stars

---
+++ src/Star_Pattern_Printing/Question5.java
@@ -0,0 +1,18 @@
+package Star_Pattern_Printing;
+
+import java.util.Scanner;
+
+//print an increasing triangle of stars
+public class Question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
