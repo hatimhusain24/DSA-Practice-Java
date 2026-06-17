d243b2d8a327c702713dbb8dac2a489025b65037
Hatim Husain <hatim.husain24@gmail.com>
2026-06-17 17:44:14 +0530

    Question 23 Star Pattern Printing : print the increasing and decreasing pattern of stars but nth row should be repeated * ** *** *** ** *

---
+++ src/Star_Pattern_Printing/Question23.java
@@ -0,0 +1,26 @@
+package Star_Pattern_Printing;
+
+import java.util.Scanner;
+
+//print the increasing and decreasing pattern of stars but nth row should be repeated * ** *** *** ** *
+public class Question23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=n; i>0; i--){
+            for(int j=i; j>0; j--){
+                System.out.print("*");
+            }
+            if (i!=1){
+                System.out.println();
+            }
+        }
+    }
+}
